617d58da879eb5287aee05308bcb33f596657abe
Stefan Pettersson <stefan.pettersson@lumano.se>
2013-12-02 16:04:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forumexp::Application.config.secret_token = '0143c9a41c4cd9b049433eec84dd40f776f7c54173e6053e63de10c5847b9850807d06f746df61e85943a19c2d9fc139bea84d80e825c88133719fc8fb4764fc'
